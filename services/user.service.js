9561a5136d3e695d70e826caeaebd1898d1d8873
Jaime Borjas <jaime.borjas@hotmail.com>
2022-01-29 13:39:37 -0500

    fix typo

--- services/user.service.js
+++ services/user.service.js
@@ -3,21 +3,21 @@ const boom = require('@hapi/boom');
 
 class UserService {
     constructor() {
-        this.products = [];
+        this.users = [];
         this.generate(100);
     }
 
     // Generates fake data to send in request via faker (NEEDS TO BE CHANGE ONCE WE FIGURE OUT THE DATABASE)
     generate(limit) {
         for (let i = 0; i < limit; i++) {
-            this.products.push({
+            this.user.push({
                 id: faker.datatype.uuid(),
                 type: "student",
                 firstName: faker.name.firstName(),
                 lastName: faker.name.lastName(),
                 image: faker.image.people(),
             });
-            this.products.push({
+            this.users.push({
                 id: faker.datatype.uuid(),
                 type: "preceptor",
                 firstName: faker.name.firstName(),
@@ -30,7 +30,7 @@ class UserService {
     // Create a new user [TO DO => how to create a user depending on the type]
     async create(data) {
         const { name, firstName, lastName, image } = data;
-        const newProduct = {
+        const newUser = {
             id: faker.datatype.uuid(),
             type: "student",
             firstName: firstName,
@@ -41,17 +41,17 @@ class UserService {
 
     // Returns all users in the fake database once we figure it out the database we will do lookups in this function
     findAll() {
-        return this.products;
+        return this.users;
     }
 
     // Returns an array of all the users who are students
     findStudents() {
-        return this.products.filter(item => item.type == "student");
+        return this.users.filter(item => item.type == "student");
     }
 
     // Returns an array of all the users who are preceptors
     findPreceptors() {
-        return this.products.filter(item => item.type == "preceptor");
+        return this.users.filter(item => item.type == "preceptor");
     }
 
 }
